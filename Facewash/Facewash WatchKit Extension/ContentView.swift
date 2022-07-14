7e6a5d7244e4a7f9dd0b72adf0dd0dbe2b1ebf3c
Nadia Ramadhani <103446529+nadiaramadhani@users.noreply.github.com>
2022-07-14 12:12:23 +0700

    NEW PROJECT

---
+++ Facewash/Facewash WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facewash WatchKit Extension
+//
+//  Created by Local Administrator on 13/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facewash/Facewash WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Facewash WatchKit Extension
+//
+//  Created by Local Administrator on 13/07/22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
